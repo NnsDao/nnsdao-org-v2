7d2b0f887dfba177abab9e7176247d4f3d7585f5
chuhemiao <team@nnsdao.com>
2023-09-17 17:54:00 +0800

    remove runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    optimizeHoistedScript: true,
-  },
   integrations: [tailwind(), compress()],
   server: { port: 3000 },
-  build: {
-    server: './server',
-  },
+  output: 'server',
+  adapter: vercel(),
 });
